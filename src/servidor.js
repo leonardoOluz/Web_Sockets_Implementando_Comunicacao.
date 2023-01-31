168fb078e10fcd14a4abe2635ba0e5f8e7801807
Leonardo Luz <leonardoluz10@hotmail.com>
2023-01-31 11:12:56 -0300

    Criando conexão com socket.io

--- src/servidor.js
+++ src/servidor.js
@@ -2,15 +2,24 @@
 import express from "express";
 import url from 'url';
 import path from "path";
+import http from 'http';
+import { Server } from "socket.io";
 /* Variáveis*/
 const app = express();
 const port = process.env.PORT || 3000
 const caminhoAtual = url.fileURLToPath(import.meta.url);
 const diretorioPublico = path.join(caminhoAtual, '../..', 'public');
-
+const servidorHttp = http.createServer(app)
 /* Passando o uso do express.static para app.use identificar a pasta public com os html */
+
 app.use(express.static(diretorioPublico));
 
-app.listen(port, ()=> {
+servidorHttp.listen(port, ()=> {
     console.log(`escuntando servidor na porta http://localhost:${port}`)
+})
+
+const io = new Server(servidorHttp);
+
+io.on('connection', ()=>{
+    console.log(`Um cliente se conectou!`)
 })
