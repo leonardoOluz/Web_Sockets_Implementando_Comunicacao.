44b73f9f9070bf1e173f9caee4cf70f36696ca0d
Leonardo Luz <leonardoluz10@hotmail.com>
2023-02-02 10:13:51 -0300

    Criando arquivo de conexão com banco MongoDB

---
+++ src/config/dbConnect.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
+
+const cliente = new MongoClient(`mongodb+srv://${dbUser}:${dbPass}@clusterleoluz.zoaawnu.mongodb.net/?retryWrites=true&w=majority`)
+
+try {
+    await cliente.connect();
+    const db = cliente.db('alura-websockets');
+    const documentos = db.collection('documentos')
+    console.log(`conectado com banco de dados com sucesso!`)
+} catch (error) {
+    console.log(error)
+}
--- src/servidor.js
+++ src/servidor.js
@@ -4,12 +4,15 @@ import url from 'url';
 import path from "path";
 import http from 'http';
 import { Server } from "socket.io";
+import './config/dbConnect.js'
+
 /* Variáveis*/
 const app = express();
 const port = process.env.PORT || 3000
 const caminhoAtual = url.fileURLToPath(import.meta.url);
 const diretorioPublico = path.join(caminhoAtual, '../..', 'public');
 const servidorHttp = http.createServer(app)
+
 /* Passando o uso do express.static para app.use identificar a pasta public com os html */
 
 app.use(express.static(diretorioPublico));
